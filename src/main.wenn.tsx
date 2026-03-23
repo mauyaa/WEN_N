a42a94474e90023aac350d7e41a0b449f45a881a
mauyaa <mauyabevan@gmail.com>
2026-03-23 19:39:53 +0300

    Add WENN-first frontend entrypoint

---
+++ src/main.wenn.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WennApp from './App.wenn';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WennApp />
+  </React.StrictMode>,
+);
