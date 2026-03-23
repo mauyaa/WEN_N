894efa22652bc3d826c0dc0691171a210caa518b
mauyaa <mauyabevan@gmail.com>
2026-03-23 21:21:42 +0300

    Add WENN unified frontend entrypoint

---
+++ src/main.unified.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WennUnifiedApp from './App.unified';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WennUnifiedApp />
+  </React.StrictMode>,
+);
