9f3d5694c5c9ce6245583de7d9d4974562c24b71
mauyaa <mauyabevan@gmail.com>
2026-03-23 20:05:19 +0300

    Add WENN command-center frontend entrypoint

---
+++ src/main.command.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WennCommandApp from './App.command';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WennCommandApp />
+  </React.StrictMode>,
+);
